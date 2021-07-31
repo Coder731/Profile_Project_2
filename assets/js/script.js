d46136fd8984e5343c0c3663ff7ab0f4fa9a44cd
Coder731 <stephenobrien1@gmail.com>
2021-07-31 19:47:37 +0000

    Compress comments to one line Delete commented code

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,11 +13,7 @@
     // https://www.w3docs.com/snippets/javascript/how-to-get-a-timestamp-in-javascript.html
     let e = new Date().getTime()
 
-// Reference Comment:
-// How to convert seconds to minutes and hours in javascript
-// https://stackoverflow.com/questions/37096367/how-to-convert-seconds-to-minutes-and-hours-in-javascript
-
-// let d = 
+// Reference Comment:    // How to convert seconds to minutes and hours in javascript    // https://stackoverflow.com/questions/37096367/how-to-convert-seconds-to-minutes-and-hours-in-javascript
 
     function secondsToHms(d) {
         d = Number(d);
@@ -32,16 +28,8 @@
     }
 
 secondsToHms();
-//  Reference:
-//  https://www.mediawiki.org/wiki/API:Search#JavaScript
-/*
-    search.js
-
-    MediaWiki API Demos
-    Demo of `Search` module: Search for a text or title
-
-    MIT License
-*/
+//  Reference:    //  https://www.mediawiki.org/wiki/API:Search#JavaScript
+/*  search.js    MediaWiki API Demos    Demo of `Search` module: Search for a text or title    MIT License    */
 
 var url = "https://en.wikipedia.org/w/api.php"; 
 
@@ -64,17 +52,12 @@ fetch(url)
     })
     .catch(function(error){console.log(error);});
 
-// Reference:
-// https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_search
-
-// set a html element equal to something:
+// Reference:    // set a html element equal to something:    // https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_search
 
 let str = "Visit W3Schools!"; 
 document.getElementById("output").innerHTML = str.search("W3Schools");
 
-// Reference Comment:
-// Simple Search in JavaScript [JavaScript Series]
-// https://www.youtube.com/watch?v=SWkPXbQXArk&t=1s
+// Reference Comment:    // Simple Search in JavaScript [JavaScript Series]    // https://www.youtube.com/watch?v=SWkPXbQXArk&t=1s
 
 const searchInput = document.getElementById('search');
 
@@ -91,13 +74,9 @@ function goWiki() {
         console.log("TESTING");
 }
 
-// Reference Comment:
-// Coding Challenge #75: Wikipedia API
-// https://www.youtube.com/watch?v=RPz75gcHj18
+// Reference Comment:    // Coding Challenge #75: Wikipedia API    // https://www.youtube.com/watch?v=RPz75gcHj18
 
-// Reference Comment:
-// https://gomakethings.com/listening-for-click-events-with-vanilla-javascript/#the-vanilla-js-way-to-listen-for-click-events
-// The vanilla JS way to listen for click events
+// Reference Comment:    // The vanilla JS way to listen for click events    // https://gomakethings.com/listening-for-click-events-with-vanilla-javascript/#the-vanilla-js-way-to-listen-for-click-events
 
 document.addEventListener('click', function (event) {
 
@@ -110,11 +89,7 @@ document.addEventListener('click', function (event) {
     // https://www.w3docs.com/snippets/javascript/how-to-get-a-timestamp-in-javascript.html
     console.log('clicked on search bar 2');
 
-    // log time of click:
-        // Reference Comment:
-        // Get current time in Hours24: Minutes: Seconds format in JavaScript
-        // https://www.includehelp.com/code-snippets/get-current-time-in-hours24-minutes-seconds-format-in-javascript.aspx
-        // (edited)
+    // log time of click:        // Reference Comment:        // Get current time in Hours24: Minutes: Seconds format in JavaScript        // (edited)        // https://www.includehelp.com/code-snippets/get-current-time-in-hours24-minutes-seconds-format-in-javascript.aspx
 
         var dt = new Date(); //Date constructor 
         var hh = dt.getHours();
