c685ceb7f99175796e8d46b80783fd4c5bb3634c
Coder731 <stephenobrien1@gmail.com>
2021-08-01 15:55:07 +0000

    Remove spaces

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,7 @@
     function myFunction() { var d = new Date(); var n = d.getTime(); document.getElementById("demo").innerHTML = n; }
     let e = new Date().getTime()  // (Reference (3))
 var url = "https://en.wikipedia.org/w/api.php"; 
-var params = {    action: "query",    list: "search",    srsearch: "Nelson Mandela",    format: "json"                              };
+var params = {    action: "query",    list: "search",    srsearch: "Nelson Mandela",    format: "json"    };
 url = url + "?origin=*";
 Object.keys(params).forEach(function(key){url += "&" + key + "=" + params[key];});
 fetch(url)  .then(function(response){return response.json();})
