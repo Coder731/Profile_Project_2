56ef3ac8d6a227d864e19a44ddb65e0f243c740e
Coder731 <stephenobrien1@gmail.com>
2021-08-05 00:05:05 +0000

    Correctly lint wiki Object keys function

--- assets/js/script.js
+++ assets/js/script.js
@@ -78,7 +78,10 @@ document.addEventListener('click', function (event) {
         console.log(url);
         params.srsearch=searchString;
         console.log(params.srsearch);
-        Object.keys(params).forEach(function(key){url += "&" + key + "=" + params[key];});
+        Object.keys(params).forEach(function(key){
+            url += "&" + key + "=" + params[key];
+        });
+        
         fetch(url)  .then(function(response){return response.json();})
             .then(function(response) {if (response.query.search[0].title==="Nelson Mandela"){console.log("Your search page 'Nelson Mandela' exists on English Wikipedia");  }  })
             .catch(function(error){console.log(error);});
